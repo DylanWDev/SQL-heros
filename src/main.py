b59b93f97e0a2f678384d15ac7ddf4cf77058436
Dylan Wyrick <xpoweroutage@gmail.com>
2023-11-02 22:20:53 +0000

    added selection functionality

--- src/main.py
+++ src/main.py
@@ -4,29 +4,20 @@ def select_all_heroes():
     query = """
         SELECT * FROM heroes
     """
-
     returned_items = execute_query(query)
     for item in returned_items:
-        print(item[1])
+        print(item)
     return returned_items
-
-select_all_heroes()
-
-
 
 def create_new_character(name, about_me, biography):
     check_query = "SELECT id FROM heroes WHERE name = %s;"
     result = execute_query(check_query, (name,))
+
     if not result:
         insert_query = "INSERT INTO heroes (name, about_me, biography) VALUES (%s, %s, %s);"
         execute_modify(insert_query, (name, about_me, biography))
-
-name = input("Enter the character's name: ")
-about_me = input("Enter info about the character: ")
-biography = input("Enter the character's biography: ")
-
-create_new_character(name, about_me, biography)
-
+    else:
+        print(f"Character with the name '{name}' already exists in the database.")
 
 def update_existing_character():
     name = input("Enter the character's name you want to update: ")
@@ -40,4 +31,20 @@ def update_existing_character():
         execute_modify(update_query, (about_me, biography, name))
     else:
         print(f"Character '{name}' not found in the database.")
-update_existing_character()
+
+def delete_character():
+    
+
+user_continue = True
+while user_continue:
+    func = ["create", "read", "update", "delete"]
+    select_functionality = input("CREATE, READ, UPDATE, DELETE: ").lower()
+    if select_functionality == "read":
+        select_all_heroes()
+    elif select_functionality == "create":
+        name = input("Enter the character's name: ")
+        about_me = input("Enter info about the character: ")
+        biography = input("Enter the character's biography: ")
+        create_new_character(name, about_me, biography)
+    elif select_functionality == "update":
+        update_existing_character()
